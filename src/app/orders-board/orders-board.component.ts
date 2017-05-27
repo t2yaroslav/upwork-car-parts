72a6230762313781debeabd808fcdab30cc289f8
shumik-vladyslav <shumik.vladyslav@gmail.com>
2017-05-27 22:14:32 +0300

    multi change status

--- src/app/orders-board/orders-board.component.ts
+++ src/app/orders-board/orders-board.component.ts
@@ -16,7 +16,7 @@ export class OrdersBoardComponent implements OnInit {
   suppliersFilter: string[] = ['lalavto', 'avtocompania', 'alex-avto'];
 
   orderStatuses: any[] = ['new', 'read', 'in work', 'started in 1s', 'sent to the supplier', 'came to the supplier'];
-// [(selection)]="selectedOrders" 
+
   orders: Order[] = [];
   selectedOrders: Order[] = [];
 
@@ -28,18 +28,24 @@ export class OrdersBoardComponent implements OnInit {
 
     console.log(JSON.stringify(this.orderStatuses));
     for (let index = 0; index < 9; index++) {
-         this.orders.push({oreder_id: index, order_date: '25.04.2015', code: '5413',
-    name: 'Wheel' , producer: 'Ruvile', quantity: '5',  price: '50',
-    vendor_price: '52', balance: '2', delivery_time: '1 day', status: 'new'});
+      this.orders.push({oreder_id: index, order_date: '25.04.2015', code: '5413',
+      name: 'Wheel' , producer: 'Ruvile', quantity: '5',  price: '50',
+      vendor_price: '52', balance: '2', delivery_time: '1 day', status: 'new'});
     }
   }
 
   onChangeSuppliersFilter(checked) {
-    // console.log("check box changed");
-    console.log(JSON.stringify(this.selectedValues));
+    // console.log(JSON.stringify(this.selectedValues));
   }
 
   onSelectionChange() {
-    console.log(JSON.stringify(this.selectedOrders));
+    // setTimeout(() => console.log(JSON.stringify(this.selectedOrders)), 1000);
   }
+
+  onStatusChange(order) {
+    if (this.selectedOrders.includes(order)) {
+      this.selectedOrders.forEach(element => element.status = order.status);
+    }
+  }
+
 }
